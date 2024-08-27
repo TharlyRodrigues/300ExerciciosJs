0af086d5286104570776a70668b6a6c4edf98f37
TharlyRodrigues <tharly.portifolio@gmail.com>
2024-08-27 19:41:32 -0300

    Fórmulas Matemáticas

---
+++ fórmulasMatemáticas/script.js
@@ -0,0 +1,45 @@
+// Exercicios Javascript
+
+// Q1
+// let num1 = parseFloat(prompt("Digite o primeiro  numero:"));
+// let num2 = parseFloat(prompt("Digite o segundo  numero:"));
+
+// let soma = num1 + num2;
+// let subtracao = num1 - num2;
+// let multiplicacao = num1 * num2;
+// let divisao = num1 / num2;
+
+// alert(
+//   ` soma = ${soma}\n subtração = ${subtracao}\n multiplicaçõa = ${multiplicacao}\n divisaão = ${divisao}`
+// );
+
+// Q2
+
+// let num1 = parseFloat(prompt("Digite o primeiro numero"));
+// let num2 = parseFloat(prompt("Digite o segundo numero"));
+// //media
+// let media = (num1 + num2) / 2;
+
+// alert(`media ${media}`);
+
+//Q3
+
+// let num1 = parseFloat(prompt("Digite a primeira nota"));
+// let num2 = parseFloat(prompt("Digite a segunda nota"));
+// let num3 = parseFloat(prompt("Digite a terceira nota"));
+
+// let mediaDasNotas = (num1 + num2 + num3) / 3;
+
+// alert(`media das notas = ${mediaDasNotas}`);
+
+//Q4
+
+// let num1 = parseFloat(prompt("Digite a primeira nota"));
+// let num2 = parseFloat(prompt("Digite a segunda nota"));
+// let num3 = parseFloat(prompt("Digite a terceira nota"));
+
+// let medidaGeometrica = Math.pow(num1 * num2 * num3, 1 / 3);
+
+// alert("Media Geométrica" + " " + medidaGeometrica);
+
+//Q5
